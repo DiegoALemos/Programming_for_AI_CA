d25da965f0fec74280109f50d18b350d079e73ee
DiegoAmaxmandro <102663353+DiegoAmaxmandro@users.noreply.github.com>
2024-11-01 12:01:29 +0000

    Removing JavaScript files and node_modules folder from project

--- Create_database.mongodb.js
+++
@@ -1,9 +0,0 @@
-// Creating Database and collection
-const database = "GlobalWeather_data";
-const collection = "GlobalWeather_data";
-
-// Creating a new database.
-use(database);
-
-// Creating a new collection.
-db.createCollection(collection);
